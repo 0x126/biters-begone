1bd82a28dbb94aed68ddb0aee7f86207acbd5357
John Bauer <horatio@gmail.com>
2019-11-02 00:21:06 -0700

    Still having problems with biters appearing where they shouldn't, so we have two fixes.  One is that when a chunk is charted (such as someone exploring) any biters which escaped the chunk generation wipe are re-wiped.  The other is that if a spawner creates a biter, both the biter and the spawner are immediately destroyed.  This makes it so biters never actually spread or attack even if their spawners are created despite all of the other efforts.  Released as v0.2.0

--- control.lua
+++ control.lua
@@ -6,12 +6,12 @@ local function clear_biters(area, silent)
     expanded_area = {}
     
     expanded_area["left_top"] = {}
-    expanded_area["left_top"]["x"] = area.left_top.x - 64
-    expanded_area["left_top"]["y"] = area.left_top.y - 64
+    expanded_area["left_top"]["x"] = area.left_top.x - 32
+    expanded_area["left_top"]["y"] = area.left_top.y - 32
     
     expanded_area["right_bottom"] = {}
-    expanded_area["right_bottom"]["x"] = area.right_bottom.x + 64
-    expanded_area["right_bottom"]["y"] = area.right_bottom.y + 64
+    expanded_area["right_bottom"]["x"] = area.right_bottom.x + 32
+    expanded_area["right_bottom"]["y"] = area.right_bottom.y + 32
   end
   
   for _, surface in pairs(game.surfaces) do
@@ -34,5 +34,35 @@ script.on_init(function()
 end)
 
 script.on_event(defines.events.on_chunk_generated, function(event)
+  -- RSO creates entities without looking at the entity creation
+  -- values.  This means that our data-final-fixes changes don't
+  -- turn off biter spawning when RSO biter spawning is active.
+  -- Therefore, we try to clear out any created entities in the
+  -- RSO affected region.
   clear_biters(event.area, true)
 end)
+
+script.on_event(defines.events.on_chunk_charted, function(event)
+  -- For chunks far from home, RSO has a very large area, so
+  -- some created spawners and turrets escape unless we expand
+  -- the area by an unreasonable amount.
+
+  --area = {}
+  --area["left_top"] = {}
+  --area["left_top"]["x"] = event.position.x * 32
+  --area["left_top"]["y"] = event.position.y * 32
+  --area["right_bottom"] = {}
+  --area["right_bottom"]["x"] = event.position.x * 32 + 32
+  --area["right_bottom"]["y"] = event.position.y * 32 + 32
+
+  clear_biters(event.area, true)
+end)
+
+script.on_event(defines.events.on_entity_spawned, function(event)
+  -- Of course, if a chunk is generated before it gets charted,
+  -- that means the spawner might produce a biter.  As soon as
+  -- that happens, we destroy both the spawner and the biter.
+  event.spawner.destroy()
+  event.entity.destroy()
+end)
+
--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,7 +1,38 @@
+local zeroExpression = {
+    expression_id = "literal-number:1",
+    literal_value = 0,
+    type = "literal-number"
+  }
+
+function resetRichness(ent)
+  if ent and ent.autoplace then
+    ent.autoplace.richness_multiplier = null
+    ent.autoplace.richness_expression = zeroExpression
+    ent.autoplace.probability_expression = zeroExpression
+  end
+end
+
+function removeProbability(ent)
+  if ent and ent.autoplace then
+    ent.autoplace.probability_expression = zeroExpression
+  end
+end
+
+for _, spawner in pairs(data.raw["unit-spawner"]) do
+  removeProbability(spawner)
+end
+
+for _, turret in pairs(data.raw.turret) do
+  if turret.subgroup == "enemies" then
+    removeProbability(turret)
+  end
+end
+        
 for _, type in pairs(data.raw) do
   for _, prototype in pairs(type) do
     if prototype.autoplace and prototype.autoplace.force == "enemy" then
-      prototype.autoplace.peaks = {}
+      resetRichness(prototype)
+      removeProbability(prototype)
     end
   end
 end
