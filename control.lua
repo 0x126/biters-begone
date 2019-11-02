43f6b2b705df950d1e4c93902bc08b6d1a000e12
John Bauer <horatio@gmail.com>
2019-11-02 00:16:48 -0700

    Erase biters in a larger area than the original event when generating a new chunk.  Sometimes biters are created outside the current event area

--- control.lua
+++ control.lua
@@ -1,14 +1,31 @@
 local function clear_biters(area, silent)
   local num = 0
+  local expanded_area=nil
+  
+  if area then
+    expanded_area = {}
+    
+    expanded_area["left_top"] = {}
+    expanded_area["left_top"]["x"] = area.left_top.x - 32
+    expanded_area["left_top"]["y"] = area.left_top.y - 32
+    
+    expanded_area["right_bottom"] = {}
+    expanded_area["right_bottom"]["x"] = area.right_bottom.x + 32
+    expanded_area["right_bottom"]["y"] = area.right_bottom.y + 32
+  end
+  
   for _, surface in pairs(game.surfaces) do
-    for _, entity in pairs(surface.find_entities_filtered({force="enemy", area=area})) do
+    for _, entity in pairs(surface.find_entities_filtered({force="enemy", area=expanded_area})) do
       if entity.destroy() then num = num + 1 end
     end
   end
 
   if not silent then
-    for _, force in pairs(game.forces) do force.rechart() end
     game.print({"command-output.biters_begone-init", num, {"command-output.biters_begone-wink-"..math.random(13)}})
+
+    for _, force in pairs(game.forces) do
+      force.rechart()
+    end
   end
 end
 
@@ -17,7 +34,5 @@ script.on_init(function()
 end)
 
 script.on_event(defines.events.on_chunk_generated, function(event)
-  if settings.global["rso-biter-generation"] and settings.global["rso-biter-generation"] then
-    clear_biters(event.area, true)
-  end
+  clear_biters(event.area, true)
 end)
